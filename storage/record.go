ee7293b0eb69d6794e1c53dd37b248d998790bdd
misachi <misachibrian@gmail.com>
2021-10-17 19:21:23 +0300

    Add constructor for VarLength record

--- storage/record.go
+++ storage/record.go
@@ -1,11 +1,21 @@
 package storage
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
 	"math/rand"
+	"reflect"
+	"strconv"
 	"sync"
 	"time"
+	"unsafe"
 )
 
+var ErrColumnDoesNotExist = errors.New("column does not exist")
+
+const Term = '\n'                            // Byte slice terminator
 func IsNull(bit, nullField NullField_T) bool { return (nullField & bit) < 1 }
 
 type LocationPair struct {
@@ -27,8 +37,39 @@ type VarLengthRecord struct {
 	mtx   *sync.Mutex
 }
 
-func NewVarLengthRecord() {
-	cols, size := getColType()
+func ByteArrayToInt(r io.Reader) (int, error) {
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return 0, fmt.Errorf("ByteArrayToInt io.ReadAll error: %v", err)
+	}
+	val, err := strconv.Atoi(*(*string)(unsafe.Pointer(&data)))
+	if err != nil {
+		return val, fmt.Errorf("ByteArrayToInt error: %v", err)
+	}
+	return val, nil
+}
+
+func NewVarLengthRecord(data []byte) (*VarLengthRecord, error) {
+	termIdx := bytes.IndexByte(data, Term) // first terminator - for nullfield
+	newBuf := bytes.NewBuffer(data[:termIdx])
+	nField, err := ByteArrayToInt(newBuf)
+	if err != nil {
+		return nil, fmt.Errorf("NewVarLengthRecord: %v", err)
+	}
+	locationEnd := bytes.IndexByte(data[termIdx+1:], Term)
+	location, err := setLocation(data[termIdx+1 : locationEnd+termIdx+1])
+	if err != nil {
+		return nil, err
+	}
+	recHDR := recordHeader{
+		nullField: NullField_T(nField),
+		location:  *location,
+	}
+	return &VarLengthRecord{
+		recordHeader: recHDR,
+		field:        data[locationEnd+termIdx+2:],
+		mtx:          &sync.Mutex{},
+	}, nil
 }
 
 type FixedLengthRecord struct {
@@ -37,13 +78,89 @@ type FixedLengthRecord struct {
 	mtx       *sync.Mutex
 }
 
-// getColType returns columns and the associated sizes
-func getColType() ([][]byte, []int) {
-	return [][]byte{
-		[]byte("field1"),
-		[]byte("field2"),
-		[]byte("field3"),
-	}, []int{2, 4, 8}
+const (
+	Number = iota + 1
+	String
+)
+
+type column struct {
+	name  string
+	_type string
+}
+
+func (c column) size() int {
+	return getTypeSize(c._type)
+}
+
+type columnData struct {
+	keys []column
+}
+
+func NewColumnData() columnData {
+	// returns columns and the associated types
+	return columnData{
+		keys: []column{
+			{name: "field1", _type: "int"},
+			{name: "field2", _type: "float32"},
+			{name: "field3", _type: "uint42"},
+		},
+	}
+}
+
+func (cd columnData) column(name string) (column, error) {
+	for _, key := range cd.keys {
+		if name == key.name {
+			return key, nil
+		}
+	}
+	return column{}, ErrColumnDoesNotExist
+}
+
+func (cd columnData) index(name string) (int, error) {
+	for idx, key := range cd.keys {
+		if name == key.name {
+			return idx, nil
+		}
+	}
+	return -1, ErrColumnDoesNotExist
+}
+
+func getTypeSize(name string) int {
+	var val interface{}
+	switch name {
+	case "int8":
+		val = int8(3)
+		return int(reflect.TypeOf(val).Size())
+	case "int16":
+		val = int16(3)
+		return int(reflect.TypeOf(val).Size())
+	case "int", "int32":
+		val = int32(3)
+		return int(reflect.TypeOf(val).Size())
+	case "int64":
+		val = int64(3)
+		return int(reflect.TypeOf(val).Size())
+	case "uint8":
+		val = uint8(3)
+		return int(reflect.TypeOf(val).Size())
+	case "uint16":
+		val = uint16(3)
+		return int(reflect.TypeOf(val).Size())
+	case "uint", "uint32":
+		val = uint32(3)
+		return int(reflect.TypeOf(val).Size())
+	case "uint64":
+		val = uint64(3)
+		return int(reflect.TypeOf(val).Size())
+	case "float32":
+		val = float32(3)
+		return int(reflect.TypeOf(val).Size())
+	case "float64":
+		val = float64(3)
+		return int(reflect.TypeOf(val).Size())
+	default:
+		return -1
+	}
 }
 
 // Given the field name return the index in the record
@@ -56,6 +173,44 @@ func getFieldIndex(key string) int {
 func getFieldLocation(idx int) LocationPair {
 	// TODO Change implementation once catalogue is complete
 	return LocationPair{}
+}
+
+func setLocation(lData []byte) (*[]LocationPair, error) {
+	bufSize := len(lData)
+	newBuf := make([]byte, bufSize)
+	location := make([]LocationPair, 10)
+	if numCopy := copy(newBuf, lData); numCopy != bufSize {
+		return nil, fmt.Errorf("setLocation copy error: expected to copy %d elements but got %d", bufSize, numCopy)
+	}
+	locSep := ':'   // Location separator
+	fieldSep := ',' // Separator between offset and size
+	idx := 0
+
+	for len(newBuf) > 0 {
+		locSepIdx := bytes.IndexByte(newBuf, byte(locSep))
+		fieldSepIdx := bytes.IndexByte(newBuf, byte(fieldSep))
+		if locSepIdx == -1 {
+			locSepIdx = len(newBuf)
+		}
+		offset, err := ByteArrayToInt(bytes.NewBuffer(newBuf[:fieldSepIdx]))
+		if err != nil {
+			return nil, fmt.Errorf("setLocation: Unable to set offset: %v", err)
+		}
+
+		size, err := ByteArrayToInt(bytes.NewBuffer(newBuf[fieldSepIdx+1 : locSepIdx]))
+		if err != nil {
+			return nil, fmt.Errorf("setLocation: Unable to set size: %v", err)
+		}
+
+		location[idx] = *NewLocationPair(Location_T(offset), Location_T(size))
+		idx += 1
+
+		if (locSepIdx + 1) > len(newBuf) {
+			break
+		}
+		newBuf = newBuf[locSepIdx+1:]
+	}
+	return &location, nil
 }
 
 func (v *VarLengthRecord) fieldIsNull(bitmask NullField_T) bool {
@@ -72,10 +227,38 @@ func (v *VarLengthRecord) Location(offset Location_T) *LocationPair {
 }
 
 func (v *VarLengthRecord) GetField(key string) []byte {
-	idx := getFieldIndex(key)
+	v.mtx.Lock()
+	defer v.mtx.Unlock()
+
+	var newField []byte
+	colData := NewColumnData()
+	col, err := colData.column(key)
+
+	if err != nil {
+		return nil
+	}
+
+	idx, _ := colData.index(key)
+
 	if !v.fieldIsNull(NullField_T(idx)) {
-		location := getFieldLocation(idx)
-		return v.field[location.offset : location.offset+location.size]
+		if num := getTypeSize(col._type); num < 0 {
+			location := getFieldLocation(idx)
+			copy(newField, v.field[location.offset:location.offset+location.size])
+			return newField
+		} else {
+			var fieldIdx int
+			for i := 0; i < len(v.field); i++ {
+				if fieldIdx < (idx - 1) {
+					copy(newField, v.field[i:i+bytes.Index(
+						v.field[i:], []byte{Term})])
+					return newField
+				}
+				if v.field[i] == Term {
+					fieldIdx += 1
+				}
+			}
+		}
+
 	}
 	return nil
 }
@@ -104,13 +287,17 @@ func (v *VarLengthRecord) AddField(key string, value []byte) {
 	v.mtx.Lock()
 	defer v.mtx.Unlock()
 
-	if !isNumber(value) {
-		locLen := len(v.location)
-		v.location[locLen] = LocationPair{Location_T(len(v.field)), Location_T(len(value))}
-	}
 	var bufSize Location_T
 	for _, loc := range v.location {
 		bufSize += loc.size
+	}
+	isNum := isNumber(value)
+	if !isNum {
+		locLen := len(v.field) - int(bufSize)
+		v.location[locLen] = LocationPair{Location_T(locLen), Location_T(len(value))}
+	}
+	if isNum && !bytes.Contains(value, []byte{Term}) {
+		value = append(value, Term)
 	}
 	v.field = append(
 		v.field[:len(v.field)-int(bufSize)],
@@ -135,12 +322,16 @@ func (f FixedLengthRecord) getFieldSize(idx NullField_T) int {
 }
 func (f *FixedLengthRecord) fieldIsNull(bit NullField_T) bool { return IsNull(bit, f.nullField) }
 func (f *FixedLengthRecord) GetField(key string) []byte {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+	var newField []byte
 	idx := NullField_T(getFieldIndex(key))
 	if f.fieldIsNull(idx) {
 		return nil
 	}
 	location := getFieldLocation(int(idx))
-	return f.field[location.offset : location.offset+Location_T(f.getFieldSize(idx))]
+	copy(newField, f.field[location.offset:location.offset+Location_T(f.getFieldSize(idx))])
+	return newField
 }
 
 func (f *FixedLengthRecord) AddField(key string, value []byte) {
--- storage/record_test.go
+++ storage/record_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -32,8 +33,8 @@ func TestIsNull(t *testing.T) {
 
 func TestIsNumber(t *testing.T) {
 	type valType struct {
-		buf []byte
-		parsed bool
+		givenBuf   []byte
+		wantParsed bool
 	}
 	values := []valType{
 		{[]byte("12345"), true},
@@ -51,8 +52,81 @@ func TestIsNumber(t *testing.T) {
 
 	for _, val := range values {
 		// var isNum bool
-		if isNum := isNumber(val.buf); isNum != val.parsed {
-			t.Errorf("Expected parse result to be %v for number(%s) but got %v", val.parsed, val.buf, isNum)
+		if isNum := isNumber(val.givenBuf); isNum != val.wantParsed {
+			t.Errorf("Expected parse result to be %v for number(%s) but got %v", val.wantParsed, val.givenBuf, isNum)
+		}
+	}
+}
+
+func TestGetTypeSize(t *testing.T) {
+	type valType struct {
+		given    string
+		wantSize int
+	}
+	values := []valType{
+		{"int8", 1},
+		{"int16", 2},
+		{"int", 4},
+		{"int32", 4},
+		{"int64", 8},
+		{"uint8", 1},
+		{"uint16", 2},
+		{"uint", 4},
+		{"uint32", 4},
+		{"uint64", 8},
+		{"float32", 4},
+		{"float64", 8},
+	}
+
+	for _, val := range values {
+		if size := getTypeSize(val.given); size != val.wantSize {
+			t.Errorf("Expected size: %d for type %s but got size: %d", val.wantSize, val.given, size)
+		}
+	}
+}
+
+func TestNewVarLengthRecord(t *testing.T) {
+	data := []byte("4\n0,2:2,3:5,2\nitwasyou")
+	record, err := NewVarLengthRecord(data)
+	if err != nil {
+		t.Errorf("Create record error: %v", err)
+	}
+
+	if record.nullField != 4 {
+		t.Errorf("Expected nullField to be %d but got %d", 4, record.nullField)
+	}
+	if !bytes.Equal(record.field, []byte("itwasyou")) {
+		t.Errorf("Field should be equal: %s", record.field)
+	}
+}
+
+func TestSetLocation(t *testing.T) {
+	type valType struct {
+		given     []byte
+		wantValue []LocationPair
+	}
+	values := []valType{
+		{
+			[]byte("0,2:2,3:5,2"), []LocationPair{{0, 2}, {2, 3}, {5, 2}},
+		},
+		{
+			[]byte("0,10:10,50:60,100:160,20"), []LocationPair{{0, 10}, {10, 50}, {60, 100}, {160, 20}},
+		},
+		{
+			[]byte("2,3"), []LocationPair{{2, 3}},
+		},
+	}
+	for _, val := range values {
+		loc, err := setLocation(val.given)
+		if err != nil {
+			t.Errorf("TestSetLocation: %v", err)
+		}
+
+		for i := 0; i < len(val.wantValue); i++ {
+			lp := *loc
+			if lp[i].offset != val.wantValue[i].offset || lp[i].size != val.wantValue[i].size {
+				t.Errorf("Expected offset: %d and size: %d\nbut got\noffset: %d and size: %d", val.wantValue[i].offset, val.wantValue[i].size, lp[i].offset, lp[i].size)
+			}
 		}
 	}
 }

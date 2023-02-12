2ffd2df5c4cdd3642745b9c0bfb634179db26fef
misachi <misachibrian@gmail.com>
2023-02-12 14:40:19 +0000

    Update file mode and add new record to table

--- database/db.go
+++ database/db.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/misachi/DarDB/column"
 	"github.com/misachi/DarDB/config"
+
+	// st "github.com/misachi/DarDB/storage"
 	tbl "github.com/misachi/DarDB/table"
 )
 
@@ -19,13 +21,14 @@ type DB struct {
 
 func NewDB(dbName string, cfg *config.Config) *DB {
 	dbPath := path.Join(cfg.DataPath(), dbName)
-	err := os.MkdirAll(dbPath, os.ModeDir)
+	err := os.MkdirAll(dbPath, 0750)
 	if err != nil {
 		return nil
 	}
 	return &DB{
 		name:   dbName,
 		config: cfg,
+		table:  make(map[string]*tbl.Table),
 	}
 }
 
@@ -47,24 +50,22 @@ func (db *DB) CreateTable(tblName string, cols map[string]column.SUPPORTED_TYPE,
 	schema = append(schema, varLenKeys...)
 	infoDir := path.Join(db.config.DataPath(), db.name, tblName)
 	dataDir := path.Join(db.config.DataPath(), db.name, tblName, ".meta")
-
-	err := os.MkdirAll(infoDir, os.ModeDir)
+	err := os.MkdirAll(infoDir, 0750)
 	if err != nil {
-		return nil, fmt.Errorf("CreateTable: MkdirAll error %v", err)
+		return nil, fmt.Errorf("CreateTable: MkdirAll infoDir error %v", err)
+	}
+	err = os.MkdirAll(dataDir, 0750)
+	if err != nil {
+		return nil, fmt.Errorf("CreateTable: MkdirAll dataDir error %v", err)
 	}
 
-	err = os.MkdirAll(dataDir, os.ModeDir)
-	if err != nil {
-		return nil, fmt.Errorf("CreateTable: MkdirAll error %v", err)
-	}
-
-	dataFile, err := openRWCreate(path.Join(dataDir, tblName))
+	dataFile, err := openRWCreate(path.Join(dataDir, fmt.Sprintf("%s.data", tblName)))
 	if err != nil {
 		return nil, fmt.Errorf("CreateTable: data file error %v", err)
 	}
 	defer dataFile.Close()
 
-	infoFile, err := openRWCreate(path.Join(infoDir, tblName))
+	infoFile, err := openRWCreate(path.Join(infoDir, fmt.Sprintf("%s.data", tblName)))
 	if err != nil {
 		return nil, fmt.Errorf("CreateTable: meta file error %v", err)
 	}
@@ -80,6 +81,26 @@ func (db *DB) CreateTable(tblName string, cols map[string]column.SUPPORTED_TYPE,
 	return tb, nil
 }
 
-func (db *DB) AddRecord(tbl *tbl.Table, data map[string][]byte) bool {
-	return false
+func (db *DB) AddRecord(tbl *tbl.Table, data map[string][]byte) error {
+	fields := make([]column.Column, 0)
+	fieldVals := make([][]byte, 0)
+	columns := tbl.GetInfo().Column
+	for field, val := range data {
+		for _, col := range columns {
+			if col.Name == field {
+				fields = append(fields, col)
+				fieldVals = append(fieldVals, val)
+				break
+			}
+		}
+	}
+	_, err := tbl.AddRecord(fields, fieldVals)
+	if err != nil {
+		return fmt.Errorf("DB AddRecord: %v\n", err)
+	}
+	return nil
 }
+
+// func (db *DB) GetRecord(fieldKey string, fieldVal []byte) st.VarLengthRecord {
+
+// }

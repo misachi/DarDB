47e8940f87f0d029320a9edc2ca6589a8c3fd6bd
misachi <misachibrian@gmail.com>
2023-09-05 23:04:54 +0300

    Remove lock entry type
    Use RWMutex

--- storage/lock.go
+++ storage/lock.go
@@ -11,41 +11,16 @@ const (
 	NO_LOCK
 )
 
-// type LockEntry struct {
-// 	lockType int
-// 	lock     *Lock
-// 	value    interface{}
-// }
-
-// func NewLockEntry(lock *Lock, lType int, val interface{}) *LockEntry {
-// 	return &LockEntry{
-// 		lock:     lock,
-// 		lockType: lType,
-// 		value:    val,
-// 	}
-// }
-
-// func (e *LockEntry) UnlockEntry() error {
-// 	if e.lockType == EXCLUSIVE_LOCK {
-// 		e.lock.releaseExclusiveLock()
-// 	} else if e.lockType == SHARED_LOCK {
-// 		e.lock.releaseSharedLock()
-// 	} else {
-// 		return fmt.Errorf("UnlockEntry: unknown lock type")
-// 	}
-// 	return nil
-// }
-
 type Lock struct {
 	sharedLockCount uint
 	lockType        int
-	eLock           *sync.Mutex
+	eLock           *sync.RWMutex
 }
 
 func NewLock() *Lock {
 	return &Lock{
 		sharedLockCount: 0,
-		eLock:           &sync.Mutex{},
+		eLock:           &sync.RWMutex{},
 	}
 }
 

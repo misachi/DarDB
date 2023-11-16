b8b4b56327116f71c3222fc334e6c9f2d7182a5c
misachi <misachibrian@gmail.com>
2023-11-16 22:51:30 +0000

    Add buffersize to config options

--- config/config.go
+++ config/config.go
@@ -1,15 +1,33 @@
 package config
 
 type Config struct {
-	dataPath string
+	bufferSize    uint64
+	walBufferSize uint64
+	dataPath      string
+	walPath       string
 }
 
-func NewConfig(path string) *Config {
+func NewConfig(path, walPath string, bufSz, walBufSz uint64) *Config {
 	return &Config{
 		dataPath: path,
+		walPath: walPath,
+		bufferSize: bufSz,
+		walBufferSize: walBufSz,
 	}
+}
+
+func (c Config) BufferSize() uint64 {
+	return c.bufferSize
+}
+
+func (c Config) WalBufferSize() uint64 {
+	return c.walBufferSize
 }
 
 func (c Config) DataPath() string {
 	return c.dataPath
+}
+
+func (c Config) WalDataPath() string {
+	return c.walPath
 }

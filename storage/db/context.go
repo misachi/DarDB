f02c391f6bdfdcd39ef9d826f89e87d3ded5300c
misachi <misachibrian@gmail.com>
2024-01-15 17:37:32 +0000

    Add manager to manage all contexts
    Update context constructor to accomodate new param for `StartTransaction`

--- storage/db/context.go
+++ storage/db/context.go
@@ -2,30 +2,69 @@ package db
 
 import (
 	"fmt"
+	"log/slog"
+	"sync"
+	"sync/atomic"
 
 	cfg "github.com/misachi/DarDB/config"
 	// txn "github.com/misachi/DarDB/storage"
 )
 
+var ClientCtxMgr *ClientContextMgr
+
+type ClientContextMgr struct {
+	maxID   atomic.Uint32
+	context map[uint32]*ClientContext
+	mtx     *sync.RWMutex
+}
+
+func NewClientContextMgr() *ClientContextMgr {
+	return &ClientContextMgr{
+		context: make(map[uint32]*ClientContext),
+		mtx:     &sync.RWMutex{},
+	}
+}
+
+func GetClientContextMgr() *ClientContextMgr {
+	if ClientCtxMgr != nil {
+		return ClientCtxMgr
+	}
+	ClientCtxMgr := NewClientContextMgr()
+	return ClientCtxMgr
+}
+
+func (ctxMgr *ClientContextMgr) NewClientCtx(cfg *cfg.Config, db *DB) *ClientContext {
+	newID := ctxMgr.maxID.Add(1)
+	ctx, err := NewClientContext(newID, cfg, db)
+	if err != nil {
+		slog.Error("NewClientCtx: unable to create new context")
+		panic(err)
+	}
+	ctxMgr.context[newID] = ctx
+	return ctx
+}
+
 type ClientContext struct {
+	ctxID      uint32
 	currentTxn *Transaction
 	txnMgr     *TransactionManager
 	config     *cfg.Config
 	database   *DB
 }
 
-func NewClientContext(cfg *cfg.Config, db *DB) (*ClientContext, error) {
+func NewClientContext(ctxID uint32, cfg *cfg.Config, db *DB) (*ClientContext, error) {
 	txnMgr := NewTxnManager()
-	transaction, err := txnMgr.StartTransaction()
+	ctx := &ClientContext{}
+	transaction, err := txnMgr.StartTransaction(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("NewClientContext: Unable to create new transaction")
+		return nil, fmt.Errorf("NewClientContext: Unable to create new context")
 	}
-	return &ClientContext{
-		currentTxn: transaction,
-		txnMgr:     txnMgr,
-		config:     cfg,
-		database:   db,
-	}, nil
+	ctx.currentTxn = transaction
+	ctx.txnMgr = txnMgr
+	ctx.config = cfg
+	ctx.database =db
+	ctx.ctxID = ctxID
+	return ctx, nil
 }
 
 func (ctx *ClientContext) Close() {
